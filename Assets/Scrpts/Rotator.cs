60de3f6fb191709d76bf234790bfacdc9174df56
Mattyhin <mettyzebou@gmail.com>
2026-02-21 21:19:18 +0300

    rev2

--- Assets/Scrpts/Rotator.cs
+++ Assets/Scrpts/Rotator.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     public float speed = 30;
-    
+
     private void Update()
     {
-        transform.Rotate(new Vector3(0, 0, -1 * speed * Time.deltaTime * Input.GetAxis("Horizontal")));
+        float input = Input.GetAxis("Horizontal");
+        
+        if (Input.GetMouseButton(0))
+        {
+            input = (Input.mousePosition.x / Screen.width) * 2 - 1;
+        }
+
+        transform.Rotate(0, 0, -1 * speed * Time.deltaTime * input);
     }
 }
